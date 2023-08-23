ee0de1a38c0f255878252bde6fbe7567401b4f5b
Rosendo Martinez <rosendomartinez270@gmail.com>
2023-08-23 17:12:22 -0500

    Create tests for Calculator object constructor

---
+++ src/calculator.js
@@ -0,0 +1,11 @@
+function Calculator() {}
+
+Calculator.prototype.add = function(a,b) {}
+
+Calculator.prototype.sub = function(a,b) {}
+
+Calculator.prototype.mul = function(a,b) {}
+
+Calculator.prototype.div = function(a,b) {} 
+
+export {Calculator}
---
+++ src/calculator.test.js
@@ -0,0 +1,19 @@
+import { Calculator } from "./calculator"
+
+const calcObj = new Calculator()
+
+test('1 + 2 to equal 3', () => {
+    expect(calcObj.add(1,2)).toBe(3);
+});
+
+test('4 - 9 to equal -5', () => {
+    expect(calcObj.sub(4,9)).toBe(-5);
+});
+
+test('4 * 5 to equal 20', () => {
+    expect(calcObj.mul(4,5)).toBe(20);
+});
+
+test('8 / 4 to equal 2', () => {
+    expect(calcObj.div(8,4)).toBe(2);
+});
