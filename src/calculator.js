5ca389fbbee1234a766116f7bb36e6428ea0e429
Rosendo Martinez <rosendomartinez270@gmail.com>
2023-08-23 17:13:40 -0500

    Implement Calculator object constructor

--- src/calculator.js
+++ src/calculator.js
@@ -1,11 +1,19 @@
 function Calculator() {}
 
-Calculator.prototype.add = function(a,b) {}
+Calculator.prototype.add = function(a,b) {
+    return a + b
+}
 
-Calculator.prototype.sub = function(a,b) {}
+Calculator.prototype.sub = function(a,b) {
+    return a - b
+}
 
-Calculator.prototype.mul = function(a,b) {}
+Calculator.prototype.mul = function(a,b) {
+    return a * b
+}
 
-Calculator.prototype.div = function(a,b) {} 
+Calculator.prototype.div = function(a,b) {
+    return a / b
+} 
 
 export {Calculator}
