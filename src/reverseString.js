fbc0fa090edb43dfa0990e546b9ac7cbf31b12f8
Rosendo Martinez <rosendomartinez270@gmail.com>
2023-08-23 14:42:12 -0500

    Implement reverseString function

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,3 +1,9 @@
-function reverseString(str) {}
+function reverseString(str) {
+    let newStr = ''
+    for (let i = str.length - 1; i > -1; i--) {
+        newStr += str[i]
+    }
+    return newStr
+}
 
 export {reverseString}
